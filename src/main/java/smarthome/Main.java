699ff1fc2943ece98b7fe8d84a626bde39e3fc78
Tommy Josépovic <tommy.josepovic@gmail.com>
2020-10-04 15:27:58 -0400

    Init

---
+++ src/main/java/smarthome/Main.java
@@ -0,0 +1,11 @@
+package smarthome;
+
+import io.javalin.Javalin;
+
+public class Main {
+    public static void main(String[] args) {
+        Javalin app = Javalin.create().start(7000);
+        app.get("/", ctx -> ctx.result("Hello World"));
+        app.get("/*", ctx -> ctx.result("404: Page not: found"));
+    }
+}
